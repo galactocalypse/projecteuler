3cace2d279870c25e23129ab89900d9981af72da
Adarsh Yagnik <adarsh_yagnik@yahoo.com>
2014-02-28 04:42:19 +0530

    Updated prob61.py

--- prob61.py
+++ prob61.py
@@ -11,70 +11,77 @@
 # Now, might want to tidy up this code.
 
 import time
-start = time.time()
-g3 = set([(x*(x+1)/2) for x in range(150) if x*(x+1)/2 >= 1000 and x*(x+1)/2 <= 9999])
-g4 = set([(x*x) for x in range(150) if x*x >= 1000 and x*x <= 9999])
-g5 = set([(x*(3*x - 1)/2) for x in range(150) if (x*(3*x - 1)/2) >= 1000 and (x*(3*x - 1)/2) <= 9999])
-g6 = set([(x*(2*x - 1)) for x in range(150) if (x*(2*x - 1)) >= 1000 and (x*(2*x - 1)) <= 9999])
-g7 = set([(x*(5*x - 3)/2) for x in range(150) if x*(5*x - 3)/2 >= 1000 and x*(5*x - 3)/2 <= 9999])
-g8 = set([(x*(3*x-2)) for x in range(150) if x*(3*x-2) >= 1000 and x*(3*x-2) <= 9999])
 
-arr = g3|g4|g5|g6|g7|g8
-arr = sorted(arr)
-
+g = [set()]*6
+arr = set()
 ctr = 0
 res = [set()]*726
-
-for a in arr:
-	m = (a%100)*100
-	barr = [x for x in arr if x >= m and x < m+100 and x != a]
-	for b in barr:
-		m = (b%100)*100
-		carr = [x for x in arr if x >= m and x < m+100 and x != a and x != b]
-		for c in carr:
-			m = (c%100)*100
-			darr = [x for x in arr if x >= m and x < m+100 and x != a and x != b and x != c]
-			for d in darr:
-				m = (d%100)*100
-				earr = [x for x in arr if x >= m and x < m+100 and x != a and x != b and x != c and x != d]
-				for e in earr:
-					m = (e%100)*100
-					a2 = int(a/100)
-					farr = [x for x in arr if x >= m and x < m+100 and x != a and x != b and x != c and x != d and x != e and x%100 == a2]
-					for f in farr:
-						res[ctr] = set([a, b, c, d, e, f])
-						ctr = ctr + 1
-print "Total: ", ctr	
-end = time.time()
-print end-start, "seconds to generate"
-start = time.time()
+ans = 0
 found = False
-for sol in res:
-	if found:
-		break
-	arr = [x for x in sol if x in g3]
-	for a in arr:
-		if found:
-			break
-		brr = [x for x in sol if x in g4 and x != a]
-		for b in brr:
-			if found:
-				break
-			crr = [x for x in sol if x in g5 and x != a and x != b]
-			for c in crr:
-				if found:
-					break
-				drr = [x for x in sol if x in g6 and x != a and x != b and x != c]
-				for d in drr:
-					if found:
-						break
-					err = [x for x in sol if x in g7 and x != a and x != b and x != c and x != d]
-					for e in err:
-						frr = [x for x in sol if x in g8 and x != a and x != b and x != c and x != d and x != e]
-						if frr:
-							print sum([a, b, c, d, e, frr[0]])
-							found = True
-							break
 
-end = time.time()
-print end-start, "seconds to process"
+def initialize():
+	global g, arr
+	g[0] = set([(x*(x+1)/2) for x in range(150) if x*(x+1)/2 >= 1000 and x*(x+1)/2 <= 9999])
+	g[1] = set([(x*x) for x in range(150) if x*x >= 1000 and x*x <= 9999])
+	g[2] = set([(x*(3*x - 1)/2) for x in range(150) if (x*(3*x - 1)/2) >= 1000 and (x*(3*x - 1)/2) <= 9999])
+	g[3] = set([(x*(2*x - 1)) for x in range(150) if (x*(2*x - 1)) >= 1000 and (x*(2*x - 1)) <= 9999])
+	g[4] = set([(x*(5*x - 3)/2) for x in range(150) if x*(5*x - 3)/2 >= 1000 and x*(5*x - 3)/2 <= 9999])
+	g[5] = set([(x*(3*x-2)) for x in range(150) if x*(3*x-2) >= 1000 and x*(3*x-2) <= 9999])
+	arr = g[0] | g[1] | g[2] | g[3] | g[4] | g[5]
+	arr = sorted(arr)
+
+def process(a, s, arr):
+	global ctr
+	m = (a%100)*100
+	s = s+[a]
+	if len(s) < 6:
+		if len(s) < 5:
+			barr = [x for x in arr if x >= m and x < m+100 and x not in s]
+		else:
+			barr = [x for x in arr if x >= m and x < m+100 and x not in s and x%100 == int(s[0]/100)]
+	else:
+		res[ctr] = set(s)
+		ctr = ctr + 1
+		return
+	for b in barr:
+		process(b, s, arr)
+
+def generate():
+	global arr
+	for a in arr:
+		process(a, [], arr)
+
+
+def compute(s, sol, i):
+	global ans, g, found
+	if found:
+		return
+	if i < 6:
+		ar = [x for x in sol if x in g[i] and x not in s]
+		for a in ar:
+			s = s + [a]
+			compute(s, sol, i+1)
+	else:
+		ans = sum(s)
+		found = True
+
+def check():
+	global res
+	for sol in res:
+		compute([], sol, 0)
+
+
+if __name__ == '__main__':
+	start = time.time()
+	initialize()
+	end = time.time()
+	print "Init in", end - start, "s"
+	start = time.time()
+	generate()
+	end = time.time()
+	print "Gen in", end - start, "s"
+	start = time.time()
+	check()
+	end = time.time()
+	print "Computed in ", end - start, "s"
+	print ans
